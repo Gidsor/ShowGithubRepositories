276228566f999f970a8ac44e8b896ff2c6dd4620
Vadim Denisov <gidsor@gmail.com>
2018-10-23 00:30:59 +0500

    Added Repositories View Model

---
+++ ShowGithubRepositories/ViewModel/RepositoriesViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  RepositoryNetworkModel.swift
+//  ShowGithubRepositories
+//
+//  Created by Vadim Denisov on 22/10/2018.
+//  Copyright © 2018 Vadim Denisov. All rights reserved.
+//
+
+import ObjectMapper
+import RxAlamofire
+import RxSwift
+import RxCocoa
+
+class RepositoriesViewModel {
+    private var repositoryName: Observable<String>
+    
+    lazy var repositoriesDriver: Driver<[Repository]> = self.fetchRepositories()
+    
+    init(nameObservable: Observable<String>) {
+        self.repositoryName = nameObservable
+    }
+    
+    private func fetchRepositories() -> Driver<[Repository]> {
+        return repositoryName
+            .observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+            .flatMapLatest { text -> Observable<(HTTPURLResponse, Any)> in
+                let link = text != "" ? GitHubAPI.allPublicRepositories + "?since=\(Int(text)! - 1)" : GitHubAPI.allPublicRepositories
+                print(link)
+                return RxAlamofire
+                    .requestJSON(.get, link)
+                    .catchError { error in
+                        return Observable.never()
+                }
+            }
+            .observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+            .map { (response, json) -> [Repository] in
+                if let repos = Mapper<Repository>().mapArray(JSONObject: json) {
+                    return repos
+                } else {
+                    return []
+                }
+            }
+            .observeOn(MainScheduler.instance)
+            .do(onNext: { response in
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            })
+            .asDriver(onErrorJustReturn: [] as [Repository])
+    }
+}
