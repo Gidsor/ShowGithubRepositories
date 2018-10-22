b599c11d67efb8f4533bb24be6fbd59c21cc5d52
Vadim Denisov <gidsor@gmail.com>
2018-10-22 19:49:14 +0500

    Added cell for repository

---
+++ ShowGithubRepositories/View/RepositoryCell.swift
@@ -0,0 +1,16 @@
+//
+//  RepositoryCell.swift
+//  ShowGithubRepositories
+//
+//  Created by Vadim Denisov on 22/10/2018.
+//  Copyright © 2018 Vadim Denisov. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryCell : UITableViewCell {
+    @IBOutlet weak var id: UILabel!
+    @IBOutlet weak var login: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var descriptionRepository: UILabel!
+}
--- ShowGithubRepositories/ViewController/RepositoriesViewController.swift
+++ ShowGithubRepositories/ViewController/RepositoriesViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class RepositoriesViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
 
 }
 
